d4b6ab2a5fbd4bff461d4b82988fdbaa26ec5fce
Romain Bignon <romain@budget-insight.com>
2016-07-24 11:34:46 +0200

    add reflect of database and create declarative classes

--- clitodb/database.py
+++ clitodb/database.py
@@ -1,5 +1,13 @@
+import builtins
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session, mapper
+from sqlalchemy.schema import MetaData
+#import pdb
+
+
+class BaseTable(object):
+    pass
 
 
 class Database(object):
@@ -7,3 +15,18 @@ class Database(object):
         self.url = url
         self.engine = create_engine(url, pool_recycle=3600)
         self.Session = scoped_session(sessionmaker(bind=self.engine))
+        builtins.Session = self.Session
+        self.reflect()
+
+    def reflect(self):
+        metadata = MetaData(self.engine)
+        metadata.reflect(self.engine)
+        for name, table in metadata.tables.items():
+            tableClass = type(str(table.fullname), (BaseTable,), {})
+            try:
+                mapper(tableClass, table)
+            except:
+                continue
+            else:
+                setattr(builtins, name, tableClass)
+        #pdb.set_trace()
